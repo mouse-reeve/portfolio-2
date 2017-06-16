d404e56bebc879993f7bacf6652cae5e3f385e59
Mouse Reeve <mousereeve@riseup.net>
2017-06-15 18:57:19 -0700

    ascii header generator

---
+++ app.py
@@ -0,0 +1,47 @@
+''' portfolio site app '''
+from flask import Flask, render_template, request
+import math
+import random
+from ascii_art import placevalue_patterner
+
+app = Flask(__name__)
+
+def before_request():
+    ''' temp code for testing because this version of flask
+    isn't picking up template changes '''
+    app.jinja_env.cache = {}
+app.before_request(before_request)
+
+
+@app.route('/')
+def mainpage():
+    ''' render the home page '''
+    placevalue = random.randint(6, 8)
+
+    functions = [
+        [lambda x, y: x ** 2 * y ** 2, 'x^2 * y^2'],
+        [lambda x, y: x ** 2 + y ** 2, 'x^2 + y^2'],
+        [lambda x, y: x ** 4 + y ** 4, 'x^4 + y^4'],
+        [lambda x, y: 8 * ((x ** 5) * (y ** 5)), '8 * (x^4 + y^4)'],
+        [lambda x, y: 3 * ((x ** 2) - (y ** 2)), '3 * (x^2 - y^2)'],
+        [lambda x, y: 6 * ((x ** 4) - (y ** 4)), '6 * (x^4 - y^4)'],
+        [lambda x, y: 50 * math.sqrt(x + y), '50 * sqrt(x + y)'],
+        [lambda x, y: 5 * math.sqrt(x**2 + y**2), '5 * sqrt(x^2 + y^2)'],
+        [lambda x, y: 5 * math.sqrt(abs(x**2 - y**2)), '5 * sqrt(|x^2 - y^2|)'],
+    ]
+    fun = random.randint(0, len(functions) - 1)
+    function = functions[fun]
+
+    data = {
+        'ascii_header': placevalue_patterner(function[0],
+                                             128, 256,
+                                             placevalue),
+        'function': function[1],
+        'placevalue': placevalue,
+    }
+
+    return render_template('index.html', **data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4444)
---
+++ ascii_art.py
@@ -0,0 +1,17 @@
+''' print out binary place value patterns '''
+
+def placevalue_patterner(function, height, width, placevalue):
+    ''' create a visualization of a place value pattern '''
+    pad = placevalue + 20
+    visual = []
+    for i in range(height):
+        row = ''
+        for j in range(width):
+            value = function(i, j)
+            binary = '{0:b}'.format(int(value))
+            leftpad = '0' * (pad - len(binary)) + binary
+            char = leftpad[-1 * placevalue]
+            row += ' ' if char == '0' else '*'
+        visual.append(row)
+    return '\n'.join(visual)
+
