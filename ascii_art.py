4b73c7ea6fd179ba8ae9e5b1696cc6e51279eb6a
Mouse Reeve <mousereeve@riseup.net>
2017-06-17 18:11:40 -0700

    Simpler ascii drawing logic

--- ascii_art.py
+++ ascii_art.py
@@ -2,16 +2,16 @@
 
 def placevalue_patterner(function, height, width, placevalue, offset_y=0):
     ''' create a visualization of a place value pattern '''
-    pad = placevalue + 20
     visual = []
     for i in range(offset_y, offset_y + height):
         row = ''
         for j in range(width):
             value = function(i, j)
             binary = '{0:b}'.format(int(value))
-            leftpad = '0' * (pad - len(binary)) + binary
-            char = leftpad[-1 * placevalue]
-            row += ' ' if char == '0' else '*'
+            if len(binary) > placevalue and binary[-1 * placevalue] == '1':
+                row += '*'
+            else:
+                row += ' '
         visual.append(row)
     return '\n'.join(visual)
 
