208e0e334cd682950c2cdc27677116fa423bcf23
Mouse Reeve <mousereeve@riseup.net>
2023-08-05 07:29:41 -0700

    Python3 changes and black formatting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-''' portfolio site app '''
+""" portfolio site app """
 import json
 import math
 import random
@@ -8,45 +8,47 @@ from jinja2.exceptions import TemplateNotFound
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def mainpage():
-    ''' render the home page '''
+    """render the home page"""
     data = get_placevalue()
-    return render_template('index.html', **data)
+    return render_template("index.html", **data)
 
 
-@app.route('/<page>')
+@app.route("/<page>")
 def subpage(page):
-    ''' load an arbitrary subpage '''
+    """load an arbitrary subpage"""
     data = get_placevalue()
     try:
-        return render_template('%s.html' % page, **data)
+        return render_template("%s.html" % page, **data)
     except TemplateNotFound:
-        return render_template('notfound.html', **data), 404
+        return render_template("notfound.html", **data), 404
 
-@app.route('/placevalue/<function_id>/<placevalue>')
+
+@app.route("/placevalue/<function_id>/<placevalue>")
 def get_ascii(function_id, placevalue):
-    ''' endpoint to get ascii art data object '''
+    """endpoint to get ascii art data object"""
     return json.dumps(get_placevalue(fun=function_id, placevalue=placevalue))
 
 
 def get_placevalue(fun=None, placevalue=None):
-    ''' load a placevalue function '''
+    """load a placevalue function"""
     placevalue = random.randint(6, 8) if placevalue is None else placevalue
 
     functions = [
-        [lambda x, y: x ** 2 * y ** 2, 'x^2 * y^2'],
-        [lambda x, y: x ** 2 + y ** 2, 'x^2 + y^2'],
-        [lambda x, y: x ** 4 + y ** 4, 'x^4 + y^4'],
-        [lambda x, y: 8 * ((x ** 5) * (y ** 5)), '8 * (x^4 + y^4)'],
-        [lambda x, y: 3 * ((x ** 2) - (y ** 2)), '3 * (x^2 - y^2)'],
-        [lambda x, y: 6 * ((x ** 4) - (y ** 4)), '6 * (x^4 - y^4)'],
-        [lambda x, y: 50 * math.sqrt(x + y), '50 * sqrt(x + y)'],
-        [lambda x, y: 5 * math.sqrt(x**2 + y**2), '5 * sqrt(x^2 + y^2)'],
-        [lambda x, y: 5 * math.sqrt(abs(x**2 - y**2)), '5 * sqrt(|x^2 - y^2|)'],
-        [lambda x, y: ((x ** y) + (y ** x)) * 8, '8 * (x^y + y^x)'],
-        [lambda x, y: ((x ** y) * (y ** x)) * 4, '4 * (x^y * y^x)'],
-        [lambda x, y: ((x ** y) - (y ** x)) * 8, '8 * (x^y - y^x)'],
+        [lambda x, y: x**2 * y**2, "x^2 * y^2"],
+        [lambda x, y: x**2 + y**2, "x^2 + y^2"],
+        [lambda x, y: x**4 + y**4, "x^4 + y^4"],
+        [lambda x, y: 8 * ((x**5) * (y**5)), "8 * (x^4 + y^4)"],
+        [lambda x, y: 3 * ((x**2) - (y**2)), "3 * (x^2 - y^2)"],
+        [lambda x, y: 6 * ((x**4) - (y**4)), "6 * (x^4 - y^4)"],
+        [lambda x, y: 50 * math.sqrt(x + y), "50 * sqrt(x + y)"],
+        [lambda x, y: 5 * math.sqrt(x**2 + y**2), "5 * sqrt(x^2 + y^2)"],
+        [lambda x, y: 5 * math.sqrt(abs(x**2 - y**2)), "5 * sqrt(|x^2 - y^2|)"],
+        [lambda x, y: ((x**y) + (y**x)) * 8, "8 * (x^y + y^x)"],
+        [lambda x, y: ((x**y) * (y**x)) * 4, "4 * (x^y * y^x)"],
+        [lambda x, y: ((x**y) - (y**x)) * 8, "8 * (x^y - y^x)"],
     ]
     if fun is not None and (fun < 0 or fun > len(functions) - 1):
         fun = None
@@ -54,13 +56,12 @@ def get_placevalue(fun=None, placevalue=None):
     function = functions[fun]
 
     return {
-        'ascii_header': placevalue_patterner(
-            function[0], 512, 512, placevalue),
-        'function': function[1],
-        'placevalue': placevalue,
-        'function_id': fun,
+        "ascii_header": placevalue_patterner(function[0], 512, 512, placevalue),
+        "function": function[1],
+        "placevalue": placevalue,
+        "function_id": fun,
     }
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=4444)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=4444)
